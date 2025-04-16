bd995c30fd2cc029d28e555b875ec1cda1c09edc
pradeesh0 <pradeeshcbe0@gmail.com>
2025-04-17 00:47:17 +0530

    frontend build files added

---
+++ Blitzchess-backend/src/main/java/com/Chess/Controller/ForwardController.java
@@ -0,0 +1,13 @@
+package com.Chess.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ForwardController {
+
+    @RequestMapping(value = "/{[path:[^\\.]*}")
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
